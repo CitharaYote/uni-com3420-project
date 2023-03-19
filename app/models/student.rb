8c0d9bc509934a6d609e8959213bb8060f36f6a9
James Oyaide <j.oyaide@sheffield.ac.uk>
2023-03-19 21:39:12 +0000

    first attempt at filling db

---
+++ app/models/student.rb
@@ -0,0 +1,12 @@
+# == Schema Information
+#
+# Table name: students
+#
+#  regID      :bigint           not null
+#  status     :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Student < ApplicationRecord
+
+end
