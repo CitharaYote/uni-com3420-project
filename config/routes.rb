db8babd2d5fd4dc94661718811b9489ce9cb09a4
Chengyu Fu <cfu10@sheffield.ac.uk>
2023-03-20 17:45:32 +0000

    profile page with meaningless information, change the root to profile page, add profile button in navigation bar(not working for now)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  root "pages#home"
+  # Defines the root path route ("/")s
+  root "pages#profile"
 end
