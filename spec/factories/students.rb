8bbc2b5076c19a9e0c90bd397c92806360800720
aca21kj <kjoshi3@sheffield.ac.uk>
2023-03-23 16:22:25 +0000

    Changed the style colour for the add staff page

--- spec/factories/students.rb
+++ spec/factories/students.rb
@@ -2,13 +2,10 @@
 #
 # Table name: students
 #
-#  id           :bigint           not null, primary key
-#  module_mark  :integer
-#  reg_no       :integer
-#  total_credit :integer
-#  wmg          :decimal(, )
-#  created_at   :datetime         not null
-#  updated_at   :datetime         not null
+#  regID      :bigint           not null
+#  status     :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
 #
 FactoryBot.define do
   factory :student do
