e42a9c4876849962fb28abf07ce11646bf536dfb
jluo37@sheffield.ac.uk <jluo37@sheffield.ac.uk>
2023-03-23 13:10:47 +0000

    regular push

--- db/schema.rb
+++ db/schema.rb
@@ -66,15 +66,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_22_044216) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "students", force: :cascade do |t|
-    t.integer "reg_no"
-    t.integer "module_mark"
-    t.integer "total_credit"
-    t.decimal "wmg"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.integer "sign_in_count", default: 0, null: false
