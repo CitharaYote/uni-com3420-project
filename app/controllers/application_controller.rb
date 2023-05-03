ca5389c65223004a2a21e789c6101ac85f92eebf
Yupeng MAO <ymao24@sheffield.ac.uk>
2023-05-03 16:19:18 +0100

    handle zero divison error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,10 @@ class ApplicationController < ActionController::Base
     redirect_back(fallback_location: root_path, alert: exception.message)
   end
 
+  rescue_from ZeroDivisionError do |exception|
+    render template: "errors/error_page"
+  end
+
   private
     def update_headers_to_disable_caching
       response.headers['Cache-Control'] = 'no-cache, no-cache="set-cookie", no-store, private, proxy-revalidate'
