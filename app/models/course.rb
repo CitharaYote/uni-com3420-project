c6d6222774f06bb8efc8ab160fd5c9c308c3759c
Xueyi02 <xhuang75@sheffield.ac.uk>
2023-05-04 17:15:00 +0100

    Create or update course and generate notifications

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,7 +4,8 @@
 #
 #  id          :bigint           not null, primary key
 #  credit      :integer
-#  module_code :string
+#  module_code :string           not null
+#  title       :string
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #
@@ -18,4 +19,12 @@ class Course < ApplicationRecord
     has_many :notifications
     validates :module_code, :format => {with: /\A[a-zA-Z0-9]+\z/, message: "should only have characters from A-z and 0-9. Example: COM2009"}
     validates :credit, :length => {:minimum => 1, :message => " field empty,"}
+
+    after_create do |course|
+        Notification.create!(identifier: course.module_code, alert: "Create new course(#{course.module_code})", isModule: true, course_id: course.id)
+    end
+
+    after_update do |course|
+        Notification.create!(identifier: course.module_code, alert: "Update course(#{course.module_code})", isModule: true, course_id: course.id)
+    end
 end
