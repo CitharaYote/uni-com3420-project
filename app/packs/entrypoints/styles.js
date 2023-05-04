9b8fdbe14d86bd81c325dab407380aa033626ee4
CitharaYote <49842424+CitharaYote@users.noreply.github.com>
2023-05-04 12:18:27 +0100

    changed table onClick

--- app/packs/entrypoints/styles.js
+++ app/packs/entrypoints/styles.js
@@ -2,6 +2,6 @@ require.context("../images", true);
 
 import "bootstrap-icons/font/bootstrap-icons";
 import "../styles/twitter_bootstrap";
-import "../styles/custom";
 import "../styles/layout";
 import "../styles/tailwind";
+import "../styles/custom";
