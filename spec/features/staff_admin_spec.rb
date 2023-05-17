0ea5802c6f0b3c790c164f9c83b7f5dcaf0b7cf6
Chengyu Fu <cfu10@sheffield.ac.uk>
2023-05-17 13:20:34 +0100

    Refactoring codes, add new testing to staff page

--- spec/features/staff_admin_spec.rb
+++ spec/features/staff_admin_spec.rb
@@ -1,13 +1,37 @@
 require 'rails_helper'
 
 RSpec.feature "Visit the staff page with admin account", type: :feature do
-    scenario "could see manage buttons" do
-        admin = FactoryBot.create(:staff, :admin)
-        admin_user = FactoryBot.create(:user, username: admin.username)
+    let(:admin) {FactoryBot.create(:staff, :admin)}
+    let(:admin_user) {FactoryBot.create(:user, username: admin.username)}
+    
+    before do
         login_as(admin_user)
         visit staffs_path
+    end
+
+    scenario "could see manage buttons" do
         expect(page).to have_content(admin_user.username)
         expect(page).to have_content("Edit")
         expect(page).to have_content("Remove")
+    end
+
+    scenario "could see staff editing page" do
+        click_link "Edit"
+        expect(page).to have_content("Save")
+        expect(page).to have_content(admin_user.username)
+        expect(page).to have_content("Back")
+    end
+
+    scenario "could back to staff page from editing page" do
+        click_link "Edit"
+        click_link "Back"
+        expect(current_path).to eq(staffs_path)
+    end
+
+    scenario "could change staff details" do
+        click_link "Edit"
+        fill_in 'Username', with: 'test'
+        click_button "Save"
+        expect(page).to have_content("test")
     end
 end
--- spec/features/staff_staff_spec.rb
+++ spec/features/staff_staff_spec.rb
@@ -1,11 +1,15 @@
 require 'rails_helper'
 
 RSpec.feature "Visit the staff page with staff account", type: :feature do
-    scenario "could only read" do
-        staff = FactoryBot.create(:staff, :default)
-        staff_user = FactoryBot.create(:user, username: staff.username)
+    let(:staff) {FactoryBot.create(:staff, :default)}
+    let(:staff_user) {FactoryBot.create(:user, username: staff.username)}
+
+    before do
         login_as(staff_user)
         visit staffs_path
+    end
+
+    scenario "could only read" do
         expect(page).to have_content(staff_user.username)
         expect(page).to have_no_content("Edit")
         expect(page).to have_no_content("Remove")
