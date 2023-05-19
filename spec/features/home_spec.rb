f17763572c4d8791630e30c1b04af6d298c11f0d
Chengyu Fu <cfu10@sheffield.ac.uk>
2023-05-19 03:09:26 +0100

    update for main page testing

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -10,6 +10,12 @@ RSpec.feature 'Home page', type: :feature do
     visit students_path
     page.attach_file('file',"#{Rails.root}/spec/features/test_file/Student module marks - MUST35 - MUS642 (60).csv")
     click_button 'Upload Marks'
+    page.attach_file('file',"#{Rails.root}/spec/features/test_file/Student module marks - MUST50 - MUS650 (60).csv")
+    click_button 'Upload Marks'
+    visit programs_path
+    click_link 'Edit'
+    check 'MUS650'
+    click_button 'Save'
     visit root_path
   end
 
@@ -20,13 +26,21 @@ RSpec.feature 'Home page', type: :feature do
   end
 
   scenario "User can filter by module" do
-    check 'MUS642'
-    click_button "Submit"
-    expect(page).to have_content("MUST35")
+    checkbox = find('input[type="checkbox"][value="MUS650"]')
+    checkbox.check
+    click_button 'Submit'
+    expect(page).to have_no_content('MUS640')
   end
 
-  scenario "User can print the table" do
-    find("#print_button").click
-    expect(page).to have_content("Printer")
+  #WIP
+  scenario "User can filter by programme" do
+    select('MUST35', from: 'ProgramThing_PrgmSelect', visible: false)
+    expect(page).to have_no_content('MUST50')
+  end
+
+  #WIP
+  scenario "User can click on elements in the main table to see more information" do
+    find('.bi-person-fill').click
+    expect(page).to have_css('.fixed.w-2/3')
   end
 end
