7fefc0a1adf85ccc5e0f97e49f8425fd0429eb8d
Chengyu Fu <cfu10@sheffield.ac.uk>
2023-05-19 01:42:26 +0100

    update staff_staff testing

--- spec/features/staff_staff_spec.rb
+++ spec/features/staff_staff_spec.rb
@@ -2,19 +2,13 @@
 
 require 'rails_helper'
 
-RSpec.feature 'Visit the staff page with staff account', type: :feature do
+RSpec.feature 'Login with staff account', type: :feature do
   let(:staff) { FactoryBot.create(:staff, :default) }
   let(:staff_user) { FactoryBot.create(:user, username: staff.username) }
 
-  before do
+  scenario 'should not see the staff page link' do
     login_as(staff_user)
     visit '/'
-    expect(page).to have_content('Staff')
-  end
-
-  scenario 'could only read' do
-    expect(page).to have_content(staff_user.username)
-    expect(page).to have_no_content('Edit')
-    expect(page).to have_no_content('Remove')
+    expect(page).to have_no_content('Staff')
   end
 end
